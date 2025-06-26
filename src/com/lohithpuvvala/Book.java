9031fc2855ffdbd22cfcebc81cdab7f55ebf94e0
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-26 11:52:14 +0530

    Update Book Class with Fields, Contructer, Getters & Setters

--- src/com/lohithpuvvala/Book.java
+++ src/com/lohithpuvvala/Book.java
@@ -1,4 +1,57 @@
 package com.lohithpuvvala;
 
 public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private boolean isIssued;
+
+    public Book(int id, String title, String author, boolean isIssued) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.isIssued = isIssued;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isIssued() {
+        return isIssued;
+    }
+
+    public void setIssued(boolean issued) {
+        isIssued = issued;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isIssued=" + isIssued +
+                '}';
+    }
 }
