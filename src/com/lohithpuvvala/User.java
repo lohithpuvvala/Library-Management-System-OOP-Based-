8b2d242b025ad6a0f720925334b9a00a613c6e8a
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-26 11:56:55 +0530

    Update User Class with fields, contructor, getters & setter and other key methods

--- src/com/lohithpuvvala/User.java
+++ src/com/lohithpuvvala/User.java
@@ -1,4 +1,41 @@
 package com.lohithpuvvala;
 
+import java.util.List;
+
 public class User {
+    private int userId;
+    private String name;
+    List<Book> issuedBooks;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getIssuedBooks() {
+        return issuedBooks;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void issueBook(Book book) {
+
+    }
+
+    public void returnBook(Book book) {
+
+    }
+
+    public void viewIssuedBooks() {
+
+    }
 }
