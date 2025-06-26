f81c7f19c6cc958802a288abc8a9afec016b3f09
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-26 12:07:54 +0530

    Update Main class. Create Preloaded Books to the library. Create Menu to loop until exited by User

--- src/com/lohithpuvvala/Book.java
+++ src/com/lohithpuvvala/Book.java
@@ -6,11 +6,11 @@ public class Book {
     private String author;
     private boolean isIssued;
 
-    public Book(int id, String title, String author, boolean isIssued) {
+    public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
-        this.isIssued = isIssued;
+        this.isIssued = false;
     }
 
     public int getId() {
--- src/com/lohithpuvvala/Main.java
+++ src/com/lohithpuvvala/Main.java
@@ -1,7 +1,59 @@
 package com.lohithpuvvala;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
 
+        // Preload sample books
+        library.addBook(new Book(1, "Clean Code", "Robert C. Martin"));
+        library.addBook(new Book(2, "Effective Java", "Joshua Bloch"));
+        library.addBook(new Book(3, "Design Patterns", "Erich Gamma"));
+        library.addBook(new Book(4, "The Pragmatic Programmer", "Andrew Hunt"));
+        library.addBook(new Book(5, "Introduction to Algorithms", "Cormen et al."));
+
+        // Menu Loop Starts
+        while (true) {
+            System.out.println("\n=== Library Management Menu ===");
+            System.out.println("1. Add Book");
+            System.out.println("2. Register User");
+            System.out.println("3. Issue Book");
+            System.out.println("4. Return Book");
+            System.out.println("5. View All Books");
+            System.out.println("6. View All Users");
+            System.out.println("0. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    // Add book logic
+                    break;
+                case 2:
+                    // Register user logic
+                    break;
+                case 3:
+                    // Issue book logic
+                    break;
+                case 4:
+                    // Return book logic
+                    break;
+                case 5:
+                    library.displayAllBooks();
+                    break;
+                case 6:
+                    library.displayAllUsers();
+                    break;
+                case 0:
+                    System.out.println("Exiting.... Thank you!");
+                    return;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        }
     }
 }
