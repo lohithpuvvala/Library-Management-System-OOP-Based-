203db9b36151a5c28510b521d129cd6df7d6101b
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-26 13:58:15 +0530

    Update Library. Update users and books lists to Maps to retrieve data faster

--- src/com/lohithpuvvala/Library.java
+++ src/com/lohithpuvvala/Library.java
@@ -1,29 +1,30 @@
 package com.lohithpuvvala;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Library {
-    private final List<User> users;
-    private final List<Book> books;
+    private final Map<Integer,User> users;
+    private final Map<Integer,Book> books;
     private static int bookId = 0;
     private static int userId = 0;
 
     public Library() {
-        users = new ArrayList<>();
-        books = new ArrayList<>();
+        users = new HashMap<>();
+        books = new HashMap<>();
     }
 
     public void addBook(String title, String author) {
         Book newBook = new Book(++bookId, title, author);
-        books.add(newBook);
+        books.put(bookId,newBook);
         System.out.println("=> Added:"+newBook.toString());
     }
 
     public void registerUser(String name) {
         User newUser = new User(++userId,name);
-        users.add(newUser);
+        users.put(userId,newUser);
         System.out.println("=> New User Registered: "+newUser.toString());
     }
 
@@ -36,13 +37,13 @@ public class Library {
     }
 
     public void displayAllBooks(){
-        for(Book book : books){
+        for(Book book : books.values()){
             System.out.println(book.toString());
         }
     }
 
     public void displayAllUsers(){
-        for (User user : users){
+        for (User user : users.values()){
             System.out.println(user.toString());
         }
     }
