33b98e25e66303771bd4e4229c17d6fe1f6d8ba9
lohithpuvvala <lohithpuvvala@gmail.com>
2025-06-26 12:01:42 +0530

    Update Library Class with fields, contructor, getters & setter and other key methods

--- src/com/lohithpuvvala/Library.java
+++ src/com/lohithpuvvala/Library.java
@@ -1,4 +1,38 @@
 package com.lohithpuvvala;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+    private List<User> users;
+    private List<Book> books;
+
+    public Library() {
+        users = new ArrayList<>();
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+
+    }
+
+    public void registerUser(User user) {
+        users.add(user);
+    }
+
+    public void issueBookToUser(int bookId, int userId) {
+
+    }
+
+    public void returnBookFromUser(int bookId, int userId) {
+
+    }
+
+    public void displayAllBooks(){
+
+    }
+
+    public void displayAllUsers(){
+
+    }
 }
